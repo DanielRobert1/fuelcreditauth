bbafd1c90fe1790170fd295c39d00e95a07df3ba
Daniel Aigbe <daniel4life1@gmail.com>
2023-02-04 17:43:01 +0100

    fix: fixed max length error for db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->registerAppConfigs();
     }
 
     /**
@@ -23,6 +26,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+         // Remove data wrap from json resource
+         JsonResource::withoutWrapping();
+    }
+
+    private function registerAppConfigs(): void
+    {
+        // Set max length for mysql db
+        Schema::defaultStringLength(191);
+
+        // For https scheme if not on local machine
+        if (config('app.env') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
